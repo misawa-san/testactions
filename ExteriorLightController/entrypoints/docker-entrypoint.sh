7c73f411059ab9829f9e452daf3ab1036536e6d4
Hiroshi Misawa <hiroshi.no.mobile@gmail.com>
2022-05-23 21:45:02 +0900

    v

--- ExteriorLightController/entrypoints/docker-entrypoint.sh
+++ ExteriorLightController/entrypoints/docker-entrypoint.sh
@@ -2,13 +2,13 @@
 
 touch ok.txt
 echo $ROS_DISTRO > ROSlog.txt
-chmod ugo+x /opt/ros/$ROS_DISTRO/setup.bash > chmodlog.txt
+chmod ugo+x /opt/ros/$ROS_DISTRO/setup.bash 2>&1 | tee chmodlog.txt
 ls -l /opt/ros/$ROS_DISTRO > optlog.txt
-source "/opt/ros/$ROS_DISTRO/setup.bash" > setuplog.txt
+source "/opt/ros/$ROS_DISTRO/setup.bash" 2>&1 | tee setuplog.txt
 cd src
-colcon build --symlink-install > colconlog.txt
-source ./install/local_setup.bash > localsetuplog.txt
-ros2 run pubsub ros_task > publog.txt &
+colcon build --symlink-install 2>&1 | tee colconlog.txt
+source ./install/local_setup.bash 2>&1 | tee localsetuplog.txt
+ros2 run pubsub ros_task 2>&1 | tee publog.txt &
 ros_pid=$!
 
 # launch server
